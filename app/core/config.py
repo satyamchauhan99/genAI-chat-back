9522a0ba4165e6774e236c9e0a2f9d5e8a9d16b8
satyamchauhan99 <51777448+satyamchauhan99@users.noreply.github.com>
2025-05-08 09:22:01 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "secret")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
